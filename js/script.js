6e221f7556842382fa3c6d8a56d7e7110479430a
yohei1755-crypto <yohei.1755@icloud.com>
2026-08-07 00:47:01 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,22 @@ document.querySelectorAll(".flip-card").forEach(card => {
     });
 
 });
+const modal = document.getElementById("imageModal");
+const img = document.getElementById("menuImage");
+const modalImg = document.getElementById("modalImg");
+const close = document.querySelector(".close");
+
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+close.onclick = function () {
+    modal.style.display = "none";
+}
+
+modal.onclick = function (e) {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+}
